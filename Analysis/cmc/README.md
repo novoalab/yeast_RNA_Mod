b7ea2360d6143577b6c2c17b695a88668ead7bf7
Oguzhan Begik <oguzhanbegik@gmail.com>
2020-11-27 18:43:13 +0100

    cmc added

---
+++ Analysis/cmc/README.md
@@ -0,0 +1,13 @@
+# Analysis of NanoCMC-seq
+
+## Calculating CMC Score and plotting
+```
+Rscript nanocmc_seq.R cmc_positions.tsv nocmc.STATS cmc.STATS
+```
+Example using test data:
+
+```
+Rscript nanocmc_seq.R cmc_positions.tsv test_data/no_cmc.STATS test_data/cmc.STATS
+```
+
+<img src="../../images/cmc/cmc.png " width="1000"> 
--- Analysis/cmc/nanocmc_seq.R
+++ Analysis/cmc/nanocmc_seq.R
@@ -7,7 +7,6 @@
 # 1. Read data and clean up (orignal vs custom)
 ####
 ####################################
-
 library(stringr)
 library(ggplot2)
 library(reshape2)
@@ -23,6 +22,132 @@ args <- commandArgs(trailingOnly = TRUE)
 cmc_positions <- args[1]#1st variable
 nocmc <- args[2]  #2nd variable
 cmc <- args[3] #3rd variable
+
+
+
+mod<- read.delim(cmc_positions)
+mod$position <- paste(mod$Chr, mod$Position, sep="_") 
+
+
+#Input
+no_cmc_input<- read.delim(nocmc, sep="")
+cmc30_input<- read.delim(cmc, sep="")
+
+
+
+
+clean_input<-function(data,label) {
+data$pos <- data$pos-13	#position fixing
+data<- subset(data, pos > 10) #Cut first 40 nucleotide
+data$position<- paste(data$chr, data$pos, sep="_")
+data$Enzyme<- rep(label, nrow(data))
+	merged<-vector() #To remove the last 40 nt
+	for (ref in (unique(data$chr))){
+		subs<- subset(data, chr==ref)
+		subs2<- subset(subs, pos< max(subs$pos)-10)
+		merged<- rbind(merged, subs2)
+	}
+# Take the coverage from +1 position
+	final <- vector()
+	for (upos in unique(merged$position)) {
+		subs <- subset(merged, position==upos)
+		numb <- subs[,"pos"] + 1 
+		plus_one <- paste(subs$chr, numb, sep="_")
+		subs_2 <- subset(data, position==plus_one)
+		subs$cov_1 <- subs_2[,"coverage"] 
+		final<- rbind(final, subs)
+	}
+
+final$norm_rt<- final$rtstop/final$cov_1 #to normalize RTdrop by the coverage at that position
+joined<- join(final,mod, by="position")
+return(joined)
+}
+
+
+
+no_cmc<-clean_input(no_cmc_input,"No_Treatment")
+cmc30<-clean_input(cmc30_input,"CMC")
+
+
+final_30 <- rbind(no_cmc,cmc30)
+
+
+
+## Delta RT-Drop
+delta_rt <- function(data_nocmc, data_cmc,label) {
+	data_nocmc2<- data_nocmc[,c("chr", "pos", "position", "norm_rt", "CMC", "Mod", "ModStatus")]
+	data_cmc2<- data_cmc[,c("position", "norm_rt")]
+	joined<- join(data_nocmc2,data_cmc2, by="position")
+	joined$delta<- abs(joined[,4]- joined[,8])
+	joined$Sample<- label
+	colnames(joined) <- c("chr","pos","position","untreated_norm_rt", "CMC", "Mod","ModStatus","treated_norm_rt", "delta", "Sample")
+	final <- vector()
+	for (ref in unique(joined$chr)){
+		subs <- subset(joined, chr==ref)
+		subs$CMC_Score <- subs$delta / median(subs$delta)
+		final <- rbind(final, subs)
+	}
+	return(final)
+}
+ 
+
+delta_30<- delta_rt(no_cmc,cmc30,"30CMC_Delta")
+write.table(delta_30, file="CMC_Scores_rRNA.tsv", sep="\t", quote=FALSE, row.names=FALSE)
+
+
+
+
+palette_15s <-c("red","gray")
+palette_21s <-c("gray")
+palette_25s <-c("gray", "steelblue")
+palette_18s <-c("gray", "steelblue")
+plot_area_rtdrop_delta_facet<- function(merged, name, ref, ylim, palette) {
+	subs <- subset(merged, chr==ref)
+	pdf(file= paste(ref,name,"_rtdrop_facet.pdf",sep=""),height=3,width=10,onefile=FALSE)
+	print(ggplot(subs, aes(x=pos, y=CMC_Score,color=Mod, fill=Mod)) +
+    geom_bar(stat="identity")
+    +ggtitle(paste(ref,name, sep="_"))
+    +ylim(0, as.numeric(ylim))
+    +scale_fill_manual(values=palette)
+    +scale_colour_manual(values=palette)
+	+geom_text_repel(data=subset(subs, CMC_Score > 25 & Mod== "Maybe"),aes(label=pos),colour="black",segment.size  = 0.4,segment.color = "grey50",size=5)
+	#+geom_text_repel(data=subset(merged, CMC_Score>25 & Mod== "No"),aes(label=pos),colour="black",segment.size  = 0.4,segment.color = "grey50",size=5)
+	+theme_classic()
+	+geom_hline(yintercept = 25, linetype="dashed")	
+	+facet_wrap(~chr, scales="free", nrow=length(unique(subs$chr))))
+	dev.off()
+}
+
+plot_area_rtdrop_delta_facet(delta_30,"Delta_Merged_30","15s", "50", palette_15s )
+plot_area_rtdrop_delta_facet(delta_30,"Delta_Merged_30","21s", "50", palette_21s )
+plot_area_rtdrop_delta_facet(delta_30,"Delta_Merged_30","25s", "210" , palette_25s)
+plot_area_rtdrop_delta_facet(delta_30,"Delta_Merged_30","18s", "210" , palette_18s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
